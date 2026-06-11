8da73e8cfed13d086bad2970a319f7907b4c51c0
cjones234gh <cjoneswa234@gmail.com>
2026-06-11 16:25:54 -0700

    And the file...

---
+++ modterm/ModtermLabel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using Windows.Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modterm
+{
+    public class ModtermLabel
+    {
+        public ModtermLabel(string textContent, bool contentSizing = true, Rect location = new Rect())
+        {
+            TextContent = textContent;
+            ContentSizing = contentSizing;
+            Location = location;
+        }
+        public Rect Location { get; set; }
+        public string TextContent { get; set; } = string.Empty;
+        public bool ContentSizing { get; set; }
+        public void Draw(CanvasControl sender, CanvasDrawingSession cds, ModtermDisplay mtd)
+        {
+            mtd.DrawModtermLabel(sender, cds, this);
+        }
+    }
+}
